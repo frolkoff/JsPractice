372038875e73d314edbfb7526beefe6252ef613f
frolkoff <markfrolkoff@gmail.com>
2022-12-20 00:34:45 +0300

    second commit

---
+++ Scripts/index.js
@@ -0,0 +1,63 @@
+// let m = {
+//     an: 2,
+//     sa: 3,
+// };
+// // alert(m.an);
+// // alert(m.sa);
+//
+// let clone = Object.assign({}, m);
+//
+// alert(clone.an)
+// alert(clone.sa)
+
+
+// let name = {name: 'j'};
+//
+// Object.assign(name, {name: 'm'});
+//
+// alert(name.name)
+
+
+// let list = {
+//     order0: 0,
+//     order1: 1200,
+//     order2: 1300,
+// };
+//
+// let ListClean = {
+// }
+//
+// ListClean = Object.assign({}, list)
+//
+// alert(ListClean.order0)
+// alert(ListClean.order1)
+// alert(ListClean.order2)
+
+
+// let user = {
+//     Name1: 'Mark',
+// };
+//
+// let user1 = {Name2: 'Anton'};
+// let user2 = {Name3: 'Alex'};
+//
+// Object.assign(user, user1, user2);
+// alert(user.Name1)
+// alert(user.Name2)
+// alert(user.Name3)
+
+
+
+// let clothes = {
+//     name: 'Mark',
+//     sizes: {
+//         height: 185,
+//         mass: 70
+//     }
+// }
+//
+// let clone = Object.assign({}, clothes);
+//
+// clothes.sizes.mass++;
+//
+// alert(clone.sizes.mass);
